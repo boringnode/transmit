365cc12abcc520d3e00a901a239f5379074373ed
Maxime <57860498+MaximeMRF@users.noreply.github.com>
2025-06-23 10:45:26 +0200

    refacto: method name

--- src/transmit.ts
+++ src/transmit.ts
@@ -235,10 +235,9 @@ export class Transmit<Context extends unknown> {
     await this.#bus?.disconnect()
   }
 
-  getAllSubscribersUUIDs(channel: string) {
+  getSubscribersFor(channel: string) {
     const subscribers = this.#manager.findByChannel(channel)
-    const uuids = Array.from(subscribers).map((subscriber) => subscriber.getUid())
-    return uuids
+    return Array.from(subscribers).map((subscriber) => subscriber.getUid())
   }
 
   #ping() {

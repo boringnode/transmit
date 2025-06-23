adb976c838048dffb411ded21486ad51562b7ac2
Maxime <57860498+MaximeMRF@users.noreply.github.com>
2025-06-23 21:01:16 +0200

    refacto(tests)

--- tests/transmit.spec.ts
+++ tests/transmit.spec.ts
@@ -405,6 +405,9 @@ test.group('Transmit', () => {
 
     const stream1 = makeStream(transmit)
     const stream2 = makeStream(transmit)
+    const stream3 = makeStream(transmit)
+    const stream4 = makeStream(transmit)
+    const stream5 = makeStream(transmit)
 
     await transmit.subscribe({
       uid: stream1.getUid(),
@@ -416,10 +419,31 @@ test.group('Transmit', () => {
       channel: 'channel1',
     })
 
-    const uuids = transmit.getSubscribersFor('channel1')
+    await transmit.subscribe({
+      uid: stream3.getUid(),
+      channel: 'channel2',
+    })
 
-    assert.lengthOf(uuids, 2)
-    assert.equal(uuids[0], stream1.getUid())
-    assert.equal(uuids[1], stream2.getUid())
+    await transmit.subscribe({
+      uid: stream4.getUid(),
+      channel: 'channel2',
+    })
+
+    await transmit.subscribe({
+      uid: stream5.getUid(),
+      channel: 'channel2',
+    })
+
+    const uuidsChannel1 = transmit.getSubscribersFor('channel1')
+    const uuidsChannel2 = transmit.getSubscribersFor('channel2')
+
+    assert.lengthOf(uuidsChannel1, 2)
+    assert.equal(uuidsChannel1[0], stream1.getUid())
+    assert.equal(uuidsChannel1[1], stream2.getUid())
+
+    assert.lengthOf(uuidsChannel2, 3)
+    assert.equal(uuidsChannel2[0], stream3.getUid())
+    assert.equal(uuidsChannel2[1], stream4.getUid())
+    assert.equal(uuidsChannel2[2], stream5.getUid())
   })
 })
